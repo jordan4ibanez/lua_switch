1ab2bcc7d1b55352a265402b69fbb657e4754097
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-03 02:51:19 -0400

    remove ecs dependency

--- switch.lua
+++ switch.lua
@@ -1,9 +1,7 @@
-require("ecs")
+-- blank class
+switch = {}
 
--- build upon existing API
-switch = ecs:new()
-
--- create a basic ecs with required internal components
+-- create a basic switch utilizing predefined table
 function switch:new(case_table)
 
     local object = {}
@@ -12,57 +10,16 @@ function switch:new(case_table)
 
     self.__index = self
 
-    self.entity_count = 0
-
-    object:add_components({
-        "case",
-        "func"
-    })
-
-    object:initialize(case_table)
+    self.case_table = case_table
 
     return object
 end
 
--- allow end programmer to implement cases
-function switch:initialize(case_table)
-
-    -- see if there is already a case for it
-    local cases = self:get_component("case")
-
-    -- run through defined cases
-    for case,defined_func in pairs(case_table) do
-
-        -- only check against existing entities
-        if self.entity_count > 0 then
-            for i = 1,self.entity_count do
-                -- don't allow duplicate cases
-                assert(cases[i] ~= case, "CASE: " .. tostring(case) .. " IS A DUPLICATE SWITCH CASE!")
-            end
-        end
-
-        -- successfully added case
-        self:add_entity({
-            case = case,
-            func = defined_func
-        })
-    end
-
-    return self
-end
-
 -- let the programmer actually use it during runtime for matching cases
 function switch:match(case)
-    -- see if there is already a case for it
-    local cases = self:get_component("case")
-    local funcs = self:get_component("func")
 
     -- only check against existing entities
-    if self.entity_count > 0 then
-        for i = 1,self.entity_count do
-            if cases[i] == case then
-                funcs[i]()
-            end
-        end
+    if self.case_table[case] then
+        self.case_table[case]()
     end
 end
