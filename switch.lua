5b391e9c4308536a9e79c770825060410d92a000
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-03 03:02:47 -0400

    make objects unique

--- switch.lua
+++ switch.lua
@@ -1,12 +1,18 @@
 -- blank class
 switch = {}
 
+local id = 0
+
 -- create a basic switch utilizing predefined table
 function switch:new(case_table)
 
     local object = {}
-    
+
     object.case_table = case_table
+
+    object.id = id
+
+    id = id + 1
 
     setmetatable(object, self)
 
